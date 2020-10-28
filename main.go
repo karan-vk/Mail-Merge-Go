286879c8fb15522a07db739abe45bb2461aa58f8
Karan <karanvknarayanan@gmail.com>
2020-10-28 15:43:16 +0530

    Fixed a bug

--- main.go
+++ main.go
@@ -1,27 +1,37 @@
 package main
 
-import ("encoding/csv"; "flag"; "io"; "os"; "text/template")
+import (
+	"encoding/csv"
+	"flag"
+	"io"
+	"os"
+	"text/template"
+)
 
 /// Wrap an array of strings as a struct, so we can pass it to a template
-type TWrap struct { Fields *[]string }
+type TWrap struct{ Fields *[]string }
 
 /// Parse a CSV so that each line becomes an array of strings, and then use
 /// the array of strings with a template to generate one file per csv line
 func main() {
 	// parse command line options
 	csvFile := flag.String("c", "list.csv", "The csv file to parse")
-	templateName := flag.String("t", "report.template", "The template to use")
+	templateName := flag.String("t", "report.tpl", "The template to use")
 	fileNameColumn := flag.Int("i", 0, "Column of csv to use as output file basename")
 	fileExt := flag.String("s", "eml", "Output file suffix")
 	flag.Parse()
 
 	// loading the template
 	template, err := template.ParseFiles(*templateName)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 
 	// loading the csv file for data
 	file, err := os.Open(*csvFile)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	defer file.Close()
 
 	// parsing the csv, one record at a time
@@ -30,7 +40,11 @@ func main() {
 	for {
 		// get next row... exit on EOF
 		row, err := reader.Read()
-		if err == io.EOF { break } else if err != nil { panic(err) }
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
 
 		// create output file for row
 		f, err := os.Create("./output/" + row[*fileNameColumn] + "." + *fileExt)

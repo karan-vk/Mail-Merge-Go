76608ff326d5e9710ca6c1b410742a8f7d95d7b8
Karan <karanvknarayanan@gmail.com>
2020-10-30 14:01:28 +0530

    Analysis

--- main.go
+++ main.go
@@ -11,40 +11,36 @@ import (
 type TWrap struct{ Fields *[]string }
 
 func main() {
-	csvFile := flag.String("c", "list.csv", "The csv file to parse")
-	templateName := flag.String("t", "report.tpl", "The template to use")
-	fileNameColumn := flag.Int("i", 0, "Column of csv to use as output file basename")
-	fileExt := flag.String("s", "eml", "Output file suffix")
-	flag.Parse()
+	csvFile := flag.String("c", "list.csv", "The csv file to parse")                   //O(1)-T O(1)-S
+	templateName := flag.String("t", "report.tpl", "The template to use")              //O(1)-T O(1)-S
+	fileNameColumn := flag.Int("i", 0, "Column of csv to use as output file basename") //O(1)-T O(1)-S
+	fileExt := flag.String("s", "eml", "Output file suffix")                           //O(1)-T O(1)-S
+	flag.Parse()                                                                       //O(1)-T O(n)-S
 
-	// loading the template
-	template, err := template.ParseFiles(*templateName)
+	template, err := template.ParseFiles(*templateName) //O(1)-T O(n)-S n - size of the template
 	if err != nil {
 		panic(err)
-	}
+	} //O(1)-T O(1)-S
 
-	// loading the csv file for data
-	file, err := os.Open(*csvFile)
+	file, err := os.Open(*csvFile) //O(1)-T O(n)-S
 	if err != nil {
 		panic(err)
-	}
-	defer file.Close()
-
-	// parsing the csv, one record at a time
-	reader := csv.NewReader(file)
+	} //O(1)-T O(1)-S
+	defer file.Close() //O(1)-T O(1)-S note space is released by the garbage collector
+	e
+	reader := csv.NewReader(file) //O(1)-T O(n)-S
 	reader.Comma = ','
 	for {
-		// get next row... exit on EOF
-		row, err := reader.Read()
+		row, err := reader.Read() //O(1)-T O(n)-S number of column
 		if err == io.EOF {
-			break
+			break //O(1)-T O(1)-S
 		} else if err != nil {
 			panic(err)
-		}
-		f, err := os.Create("./output/" + row[*fileNameColumn] + "." + *fileExt)
+		} //O(1)-T O(1)-S
+		f, err := os.Create("./output/" + row[*fileNameColumn] + "." + *fileExt) //O(1)-T O(1)-S
 
-		defer f.Close()
-		template.Execute(f, TWrap{Fields: &row})
-	}
+		defer f.Close()                          //O(1)-T O(1)-S note space is released by the garbage collector
+		template.Execute(f, TWrap{Fields: &row}) //O(1)-T O(m)-S m - number of template slots
+	} //O(n)-T O(n+m)-S
 	sendEmail()
 }
--- sendmail.go
+++ sendmail.go
@@ -7,66 +7,74 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 func sendEmail() {
-	var files []string
-	var emails []string
-	c := make(chan string)
+	var files []string     //O(1)-T O(1)-S
+	var emails []string    //O(1)-T O(1)-S
+	c := make(chan string) //O(1)-T O(1)-S
 
-	root := "./output"
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		emails = append(emails, strings.Split(path[7:], ".eml")...)
-		files = append(files, path)
+	root := "./output"                                                                //O(1)-T O(1)-S
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error { //O(n)-T O(n)-S
+		emails = append(emails, strings.Split(path[7:], ".eml")...) //O(1)-T O(1)-S
+		files = append(files, path)                                 //O(1)-T O(1)-S
 		return nil
 	})
-	if err != nil {
+	if err != nil { //O(1)-T O(1)-S
 		panic(err)
 	}
 	for index, file := range files {
-		fmt.Println(file)
+		fmt.Println(file) //O(1)-T O(1)-S
 		if index != 0 {
-			data, err := ioutil.ReadFile(file)
+			data, err := ioutil.ReadFile(file) //O(1)-T O(n)-S
 			if err != nil {
-				fmt.Println("File reading error", err)
+				fmt.Println("File reading error", err) //O(1)-T O(1)-S
 				return
 			}
-			send(emails[index], string(data), c)
+
+			go send(emails[index], string(data), c) //O(1)-T O(n)-S
+
 		}
-	}
+	} //O(n)-T O(n)-S
 	for l := range c {
 		e := l
 		go func(l string, e string) {
+			// time.Sleep(5*time.Second)
 			send(l, e, c)
 		}(l, e)
-	}
+	} //O(n)-T O(n)-S
 
+	//fmt.Println()
 }
 func send(email string, body string, c chan string) {
-	from := "testuser.sret@gmail.com"
-	password := "testuser"
+	timer1 := time.Now().UnixNano()
+	from := "testuser.sret@gmail.com" //O(1)-T O(1)-S
+	password := "testuser"            //O(1)-T O(1)-S
 
 	to := []string{
 		email,
-	}
-	fmt.Println(to[0])
+	} //O(1)-T O(1)-S
+	fmt.Println(to[0]) //O(1)-T O(1)-S
 
-	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
+	smtpHost := "smtp.gmail.com" //O(1)-T O(1)-S
+	smtpPort := "587"            //O(1)-T O(1)-S
 
-	message := []byte(body)
+	message := []byte(body) //O(1)-T O(n)-S
 
-	auth := smtp.PlainAuth("", from, password, smtpHost)
+	auth := smtp.PlainAuth("", from, password, smtpHost) //O(1)-T O(1)-S
 
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message) //O(1)-T O(n)-S
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err) //O(1)-T O(1)-S
 		c <- "err"
 		return
 	}
 
-	s := "Email to " + email + " Sent Successfully!"
-	fmt.Println(s)
+	s := "Email to " + email + " Sent Successfully!" //O(1)-T O(1)-S
+	fmt.Println(s)                                   //O(1)-T O(1)-S
+	timer2 := time.Now().UnixNano()
+	fmt.Println(timer2 - timer1)
 	c <- s
 	return
 }
